dca6b7b8994626f93cc756a306d14dc6cb79fffa
xiaojiang0318 <1339675740@qq.com>
2019-03-18 22:28:32 +0800

    third homework

---
+++ 2016302580060-肖健-第三次作业/Server.py
@@ -0,0 +1,15 @@
+from socket import *
+
+serverPort=12000
+serverSocket=socket(AF_INET,SOCK_STREAM)
+serverSocket.bind(('localhost',serverPort))
+serverSocket.listen(1)
+
+while True:
+    connectSocket,addr=serverSocket.accept()
+    
+    sentence=connectSocket.recv(1024)
+    capitalizedSentence=sentence.upper()
+    connectSocket.send(capitalizedSentence)
+    
+    connectSocket.close()
